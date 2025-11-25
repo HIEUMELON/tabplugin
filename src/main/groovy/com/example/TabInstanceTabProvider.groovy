8066321b191511e5695a7eeb1b184bd144f6b91c
HIEUMELON <101346597+HIEUMELON@users.noreply.github.com>
2025-11-25 10:10:05 +0700

    Refactor TabInstanceTabProvider to NonameInstanceTabProvider

--- src/main/groovy/com/example/TabInstanceTabProvider.groovy
+++ src/main/groovy/com/example/TabInstanceTabProvider.groovy
@@ -1,69 +1,141 @@
-// src/main/groovy/com/example/TabInstanceTabProvider.groovy
 package com.example
 
 import com.morpheusdata.core.AbstractInstanceTabProvider
 import com.morpheusdata.core.MorpheusContext
 import com.morpheusdata.core.Plugin
-import com.morpheusdata.model.Account
-import com.morpheusdata.model.Instance
-import com.morpheusdata.model.User
+import com.morpheusdata.model.*
 import com.morpheusdata.views.HTMLResponse
 import com.morpheusdata.views.ViewModel
+import groovy.util.logging.Slf4j
 
-/**
- * Instance tab provider for Morpheus SDK 8.x
- * Renders a custom tab on the Instance details page.
- *
- * Template path it renders: resources/renderer/hbs/instanceTab.hbs
- */
-class TabInstanceTabProvider extends AbstractInstanceTabProvider {
+@Slf4j
+class NonameInstanceTabProvider extends AbstractInstanceTabProvider {
+    final Plugin plugin
+    final MorpheusContext morpheus
 
-    protected final MorpheusContext morpheus
-    protected final Plugin pluginRef
-
-    TabInstanceTabProvider(Plugin plugin, MorpheusContext morpheus) {
-        this.pluginRef = plugin
-        this.morpheus = morpheus
+    NonameInstanceTabProvider(Plugin plugin, MorpheusContext context) {
+        this.plugin  = plugin
+        this.morpheus = context
     }
 
-    /**
-     * Build the HTML for this tab.
-     */
     @Override
-    HTMLResponse renderTemplate(Instance instance) {
-        // Safely read plugin settings
-        Map<String, Object> settings = morpheus.settings.getPluginSettings(pluginRef).blockingGet() ?: [:]
-        String dbUser = (settings['dbUsername'] ?: 'N/A') as String
+    String getCode() { 'noname-instance-tab' }
 
-        // ✅ Use addAttribute (SDK 8.x way)
-        ViewModel<Instance> model = new ViewModel<>()
-        model.object = instance
-        model.addAttribute('dbUsername', dbUser)
-        model.addAttribute('instanceId', instance?.id)
-        model.addAttribute('instanceName', instance?.name)
+    @Override
+    String getName() { 'Connection Details' }
 
-        // Render the handlebars template
-        return getRenderer().renderTemplate('hbs/instanceTab', model)
-    }
-
-    /**
-     * Show/hide tab for user.
-     */
     @Override
     Boolean show(Instance instance, User user, Account account) {
         return true
     }
 
-    // ---------- Required Metadata ----------
-    @Override
-    String getCode() { 'tab' }
+    // helper: convert evar value -> String
+    /*private String evarToString(Object val) {
+        if(val == null)
+            return null
+        if(val instanceof String)
+            return (String)val
+        if(val instanceof char[])
+            return new String((char[])val)
+        if(val instanceof byte[])
+            return new String((byte[])val, 'UTF-8')
+        return val.toString()
+    }*/
 
     @Override
-    String getName() { 'tab' }
+    HTMLResponse renderTemplate(Instance instance) {
+        ViewModel model = new ViewModel()
+        try {
+            TaskConfig cfg = morpheus
+                .buildInstanceConfig(instance, [:], null, [], [:])
+                .blockingGet()
+
+            TaskConfig.InstanceConfig ic = cfg?.instance
+
+            TaskConfig.ServerConfig serverCfg = ic?.containers
+                ?.collect { it?.server }
+                ?.find { it != null }
+
+            ComputeServer hypervisor = null
+            ComputeServer vmServer   = null
+
+            if(serverCfg != null) {
+                if(serverCfg.parentServerId != null) {
+                    hypervisor = morpheus.services.computeServer
+                        .get(serverCfg.parentServerId as Long)
+                }
+                if(serverCfg.id != null) {
+                    vmServer = morpheus.services.computeServer
+                        .get(serverCfg.id as Long)
+                }
+            }
+
+            String hypervisorHost =
+                    hypervisor?.externalIp ?:
+                    hypervisor?.internalIp ?:
+                    hypervisor?.name ?:
+                    'N/A'
+
+            String sshHost =
+                    vmServer?.sshHost ?:
+                    vmServer?.externalIp ?:
+                    vmServer?.internalIp ?:
+                    hypervisorHost
+
+            Integer sshPort = vmServer?.sshPort ?: 22
+            String sshUsername = vmServer?.sshUsername ?: 'cloud'
+
+            // ===== EVARS =====
+            Map evars = cfg?.getEvars() ?: [:]
+
+            // CUSTOM OPTIONS (catalog for DEMO)
+            Map customOptions = cfg?.getCustomOptions() ?: [:]
+            log.debug("NonameInstanceTab: customOptions=${customOptions}")
+            String optUsername = (customOptions['username'] ?: null) as String
+            String optPassword = (customOptions['password'] ?: null) as String
+            String optDatabase = (customOptions['database'] ?: null) as String
+            String optHost     = (customOptions['host'] ?: null) as String
+            String optPort     = (customOptions['port'] ?: null) as String
+            /*String evarUsername = evarToString(evars['username']?.value)
+            String evarPassword = evarToString(evars['password']?.value)
+            String evarDatabase = evarToString(evars['database']?.value)
+*/
+            /*String username = evarUsername ?: 'N/A'
+            String password = evarPassword ?: 'N/A'
+            String database = evarDatabase ?: 'N/A'
+*/
+            String username = optUsername ?: 'N/A'
+            String password = optPassword ?: 'N/A'
+            String database = optDatabase ?: 'N/A'
+            String dbHost   = optHost ?: sshHost
+            String dbPort   = optPort ?: sshPort.toString()
+            Map data = [
+                hypervisorHost: hypervisorHost,
+                sshHost       : sshHost,
+                sshPort       : sshPort,
+                sshUsername   : sshUsername,
+
+                username      : username,
+                password      : password,
+                database      : database,
+                host          : dbHost,
+                port          : dbPort,
+
+                instanceName  : instance?.name ?: 'Instance'
+            ]
+
+            model.object = [instance: instance, data: data]
+            return getRenderer().renderTemplate('hbs/instanceTab', model)
+
+        } catch (Throwable ex) {
+            log.error('NonameInstanceTab: render error', ex)
+            model.object = [instance: instance, data: [:]]
+            return getRenderer().renderTemplate('hbs/instanceNotFoundTab', model)
+        }
+    }
 
     @Override
-    MorpheusContext getMorpheus() { morpheus }
-
-    @Override
-    Plugin getPlugin() { pluginRef }
+    ContentSecurityPolicy getContentSecurityPolicy() {
+        new ContentSecurityPolicy()
+    }
 }
